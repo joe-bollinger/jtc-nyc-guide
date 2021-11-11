1b2dc02397f09c793b6b910b4ab7e07df3b21428
Joe Bollinger <joeboo865@gmail.com>
2021-11-11 18:15:48 -0500

    added venue.html afile add modified views.py

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # jtc-nyc-guide
+
+## Contributors:
+
+1. Joe Bollinger: https://github.com/joe-bollinger
+2. Walter Dunn Jr.: https://github.com/lavoing5762
--- nyc/views.py
+++ nyc/views.py
@@ -17,8 +17,8 @@ def borough(request, borough):
 def activity(request, borough, activity):
     if request.method == 'GET':
         return render(request=request, template_name='activity.html', context={'borough': borough, 'activity': activity, 'venues': boroughs[borough][activity]})
-    pass
 
 
-def venue():
-    pass
+def venue(request, borough, activity, venue):
+    if request.method == 'GET':
+        return render(request=request, template_name='venue.html', context={'borough': borough, 'activity': activity, 'venue': venue, 'venue_keys': boroughs[borough][activity][venue]})
